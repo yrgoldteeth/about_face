26880ac1021ee649a746de0d2448202f1ac3f2a2
Nicholas Fine <nicholas.fine@gmail.com>
2010-01-20 10:00:43 -0600

    added base sinatra app, mardown conversion library

---
+++ lib/md.rb
@@ -0,0 +1,41 @@
+require 'rubygems'
+require 'rio'
+require 'rdiscount'
+
+class Md
+
+  attr_accessor :markdown_text, :html_text, :markdown_file_path, :html_file_path
+
+  def initialize(options={})
+    @markdown_text = ''
+    @markdown_file_path = options[:markdown_file_path]
+    return false unless File.exist? @markdown_file_path
+  end
+
+  def run
+    set_html_file_path
+    get_markdown_text
+    get_html_text
+    create_html_file
+  end
+
+  def set_html_file_path
+    base_file_name = File.basename(@markdown_file_path, ".mkd")
+    base_file_path = File.dirname(@markdown_file_path)
+    @html_file_path = File.join(base_file_path, "#{base_file_name}.html")
+  end
+
+  def get_markdown_text
+    rio(@markdown_file_path) > @markdown_text
+  end
+
+  def get_html_text
+    md = RDiscount.new(@markdown_text)
+    @html_text = md.to_html
+  end
+
+  def create_html_file
+    rio(@html_file_path) < @html_text
+  end
+
+end
