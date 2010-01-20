d690e96099a95e1557e02db693a86488c355a8c8
Nicholas Fine <nicholas.fine@gmail.com>
2010-01-20 16:50:29 -0600

    changed some naming

--- about_face.rb
+++ about_face.rb
@@ -2,11 +2,13 @@ require 'rubygems'
 require 'sinatra'
 require 'rio'
 require 'rdiscount'
+require 'lib/uploaded_markdown_file_handler'
 require 'lib/md'
-require 'lib/about_face'
+
+set :root, File.dirname(__FILE__)
 
 post '/' do
-  u = UploadedMarkdownFileHandler.new(:markdown_file => params[:markdown_file])
+  u = UploadedMarkdownFileHandler.new(params)
   u.convert!
   if u.success?
     "#{u.converted_file_path}"
--- lib/uploaded_markdown_file_handler.rb
+++ lib/uploaded_markdown_file_handler.rb
@@ -1,10 +1,14 @@
 class UploadedMarkdownFileHandler
-  attr_accessor :markdown_file_path, :markdown_file_data, :success, :converted_file_path
+  attr_accessor :markdown_file_path, :markdown_file_data, :success, :converted_file_path, :return_method_type, :public_dir
 
   STORAGE_PATH = 'public/af'
 
   def initialize(options={})
     @success = false
+    return unless options[:markdown_file]
+
+    @return_method_type = options[:return_method_type] || :text
+
     uploaded_markdown_file = options[:markdown_file]
     file_name = uploaded_markdown_file[:filename]
     get_file_path(file_name)
@@ -12,10 +16,13 @@ class UploadedMarkdownFileHandler
   end
 
   def get_file_path(file_name)
-    new_directory_path = File.join(STORAGE_PATH, next_directory)
+    dir_num = next_directory
+    new_directory_path = File.join(STORAGE_PATH, dir_num)
     Dir.mkdir(new_directory_path)
 
     return unless File.directory? new_directory_path
+
+    @public_dir = File.join('ar', dir_num)
 
     @markdown_file_path = File.join(new_directory_path, file_name)
   end
@@ -34,7 +41,14 @@ class UploadedMarkdownFileHandler
     m.run
     if m.success?
       @success = true
-      @converted_file_path = m.html_file_path
+      html_file = File.basename(m.html_file_path)
+      @converted_file_path = File.join(@public_dir, html_file)
+    end
+  end
+
+  def return_method
+    case @return_method_type
+    when :text
     end
   end
 
