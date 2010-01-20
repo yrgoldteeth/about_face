799db2606396e2ac81d198aa9519d43009ff325d
Nicholas Fine <nicholas.fine@gmail.com>
2010-01-20 14:08:39 -0600

    typo in app

--- about_face.rb
+++ about_face.rb
@@ -6,7 +6,8 @@ require 'lib/md'
 require 'lib/about_face'
 
 post '/' do
-  u = UploadedMarkdownFileHandler.new(:markdown_file => params[:markdown_file]).convert!
+  u = UploadedMarkdownFileHandler.new(:markdown_file => params[:markdown_file])
+  u.convert!
   if u.success?
     "#{u.converted_file_path}"
   else
