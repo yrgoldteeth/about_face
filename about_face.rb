239d3c4cfe657ab7ca1f2bbb16d4fe1dda762fae
Nicholas Fine <nicholas.fine@gmail.com>
2010-01-20 14:04:44 -0600

    added post method to app

--- about_face.rb
+++ about_face.rb
@@ -1,3 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require 'rio'
+require 'rdiscount'
 require 'lib/md'
+require 'lib/about_face'
+
+post '/' do
+  u = UploadedMarkdownFileHandler.new(:markdown_file => params[:markdown_file]).convert!
+  if u.success?
+    "#{u.converted_file_path}"
+  else
+    "naw"
+  end
+end
