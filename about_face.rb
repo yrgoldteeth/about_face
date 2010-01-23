4662d6a4f50da2a451605d500c96a7500ca37396
Nicholas Fine <nicholas.fine@gmail.com>
2010-01-22 21:22:49 -0600

    big update

--- about_face.rb
+++ about_face.rb
@@ -11,7 +11,33 @@ require 'lib/md'
 
 set :root, File.dirname(__FILE__)
 
+helpers do
+  def lorem
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n"
+  end
+
+  def example_output
+    out = []
+    out << "#Header 1\n"
+    out << "##Header 2\n"
+    out << lorem
+    out << "###Header 3\n"
+    out << "  * #{lorem.split(' ')[0]}\n"
+    out << "  * #{lorem.split(' ')[1]}\n"
+    out << "  * #{lorem.split(' ')[2]}\n"
+    out << "  * #{lorem.split(' ')[3]}\n"
+    out << "\n"
+    out << "####Header 4\n"
+    out << "  1. #{lorem.split(' ')[3]}\n"
+    out << "  2. #{lorem.split(' ')[2]}\n"
+    out << "  3. #{lorem.split(' ')[1]}\n"
+    out << "  4. #{lorem.split(' ')[0]}\n"
+    out
+  end
+end
+
 get '/' do
+  @output = example_output
   haml :index
 end
 
@@ -21,18 +47,20 @@ end
 
 post '/' do
   #there's probably a better way, but just send a param for the response wanted.
-  params[:response_type].nil? ? reponse_type = 'html' : response_type = params[:response_type]
+  params[:response_type].nil? ? reponse_type = 'text' : response_type = params[:response_type]
 
   u = UploadedMarkdownFileHandler.new(params)
   u.convert!
 
   if u.success?
-    @markdown_file_data = u.markdown_file_data
     case response_type
-    when 'html'
-      "#{u.converted_file_data}"
+    when 'text'
+      "#{u.converted_file_data}" #return html for curl conversion
+    when 'md'
+      @output = u.markdown_file_data
+      haml :index
     else
-      haml :data
+      redirect "#{u.converted_file_path}"
     end
   else
     "naw"
